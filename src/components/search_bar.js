30e2933e74fa3d4a993e8b97e34f94b4eda97bad
Leop0ld <l3opold7@gmail.com>
2017-11-21 13:24:17 +0900

    Add state about search_bar input and show it

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 // const Component = React.Component; 라고 쓴것과 같음
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize state
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    // state 는 this.setState 메서드를 통해서만 변경해야함
+    // state 가 변경될 때마다 setState 메서드를 호출하고, 리렌더링함
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    );
   }
 }
 
