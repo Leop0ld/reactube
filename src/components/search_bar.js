7ee0f97bf5b7a3eeff2ea8f5b504383456a0baa4
Leop0ld <myungseo.kang@opengallery.co.kr>
2017-11-21 12:35:20 +0900

    Refactor search_bar component using class

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// const Component = React.Component; 라고 쓴것과 같음
+
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
